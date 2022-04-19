33d0c61ace0ded84655862dd597d7308520fd764
vlgunsdaddy <46253552+vlgunsdaddy@users.noreply.github.com>
2022-04-19 19:03:54 +0800

    back end ready

---
+++ src/main/java/com/bht/dimage/DimageApplication.java
@@ -0,0 +1,15 @@
+package com.bht.dimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimageApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DimageApplication.class, args);
+		System.out.println("Hello spring");
+	}
+
+}
---
+++ src/test/java/com/bht/dimage/DimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bht.dimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
